b43e4491d4df1ffcf0ee641b2087dc8465ac272f
silimelaL <lsilimela23@gmail.com>
2019-10-22 11:40:43 +0200

    creating posts

---
+++ resources/js/store/index.js
@@ -0,0 +1,27 @@
+import Vue from 'Vue';
+import Vuex from 'Vuex';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV === 'production';
+
+export default new Vuex.Store(
+    {
+        state: {
+            posts: [],
+        },
+
+        actions: {
+            async getAllPosts({ commit }) {
+                return commit('setPosts', await api.get('/post/get_all'))
+            },
+        },
+
+        mutations: {
+            setPosts(state, response) {
+                state.posts = response.data.data;
+            },
+        },
+        strict: debug
+    }
+);
